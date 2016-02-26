2a8b5fe285c4f7149cc778d3886cee2d8fe2854f
laurent <laurent@winXpSp2>
2016-02-26 07:54:40 +0100

    Adding original 'ssh-agent-demo.py'

--- src/kable/sample/JreInstallation.kt
+++ src/kable/sample/JreInstallation.kt
@@ -26,16 +26,17 @@ class JreInstallationPlaybook(
 
   val jreArchive = "jre" + jreVersionPrecise
 
-  val wgetCommandLine = StringBuilder()
-        .append( "wget --no-cookies --no-check-certificate " )
-        .append( "--header \"Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; " )
-        .append( "oraclelicense=accept-securebackup-cookie\" " )
-        .append( "\"http://download.oracle.com/otn-pub/java/jdk/" )
-        .append( jreVersionPrecise )
-        .append( "/" )
-        .append( jreArchive )
-        .append( "\"" )
-        .toString()
+  val wgetCommandLine = "wget --no-cookies --no-check-certificate --header \"Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie\" \"http://download.oracle.com/otn-pub/java/jdk/$jreVersionPrecise/$jreArchive\""
+//  val wgetCommandLine = StringBuilder()
+//        .append( "wget --no-cookies --no-check-certificate " )
+//        .append( "--header \"Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; " )
+//        .append( "oraclelicense=accept-securebackup-cookie\" " )
+//        .append( "\"http://download.oracle.com/otn-pub/java/jdk/" )
+//        .append( jreVersionPrecise )
+//        .append( "/" )
+//        .append( jreArchive )
+//        .append( "\"" )
+//        .toString()
 
   val jrePresent =
   - Stat( file = jreArchivesDirectory + "/" + jreArchive ) resultTransformedBy { it.exists }
